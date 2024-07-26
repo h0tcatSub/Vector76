4e40d80489a1202a7f7002ec20408979910516fe
h0tcatSub <n0ricatnohotmens@gmail.com>
2024-07-26 10:53:44 +0900

    A

--- src/soundtest.py
+++ src/soundtest.py
@@ -1,9 +1,9 @@
 import time
-import mp3play
 import subprocess
 
 sound_name = "ImagineBreaker.mp3"
 try:
+    import mp3play
     clip = mp3play.load(sound_name)
     clip.play()
     time.sleep(min(5, clip.seconds()))
