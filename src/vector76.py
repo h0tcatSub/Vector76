2c0828abd32d97c7706e947a585fdb6966df7120
h0tcatSub <n0ricatnohotmens@gmail.com>
2024-07-29 17:42:25 +0900

    a

--- src/vector76.py
+++ src/vector76.py
@@ -85,6 +85,7 @@ print("OK")
 send_amount = to_satoshi(amount_btc)
 inputs = transaction_util.unspent(transaction_util.wiftoaddr(key))
 print(inputs)
+print()
 
 if send_amount <= fee:
     print("[!] The fees exceed the amount sent. Please review the amount of fees and amount of BTC.")
